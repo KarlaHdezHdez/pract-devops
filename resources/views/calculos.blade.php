74afe668e48e3b3921b6973a9da789443d2447fa
KarlaHdezHdez <20200788@uthh.udu.mx>
2023-10-28 20:16:35 -0600

    cambios vista commit

---
+++ resources/views/calculos.blade.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8"> 
+    <title>Calculadora</title>
+    
+    <!-- Estilos -->
+    <style>        
+body {
+    padding: 0;
+    margin: 0;
+    background: linear-gradient(to right, #00AAFF, #00FF6C);
+}
+
+.calculadora-grilla {
+    display: grid;
+    justify-content: center;
+    align-content: center;
+    min-height: 100vh;
+    grid-template-columns: repeat(4, 100px);
+    grid-template-rows: minmax(120px, auto) repeat(5, 100px);
+}
+
+.calculadora-grilla > button {
+    cursor: pointer;
+    font-size: 2rem;
+    border: 1px solid white;
+    outline: none;
+    background-color: rgba(255, 255, 255, .75);
+}
+
+.output {
+    grid-column: 1 / -1;
+    background-color: rgba(0, 0, 0, .75);
+    display: flex;
+    align-items: flex-end;
+    justify-content: space-around;
+    flex-direction: column;
+    padding: 5px;
+    word-wrap: break-word;
+    word-break: break-all; 
+}
+
+.output .display {
+    color: black;
+    font-size: 1.85rem;
+    text-align: right;
+}
+
+.calculadora-grilla > button:hover {
+    background-color: rgba(255, 255, 255, .9);
+}
+
+.titulo{
+    color: #1A5276;
+    font-size: 20px;
+    font-family: arial;
+    font-weight: 900;
+    text-align: center;
+}
+.nombre{
+    text-align: center;
+    margin-right: 120px;
+}
+    </style>
+</head>
+<body>
+    <h3 class="titulo">Calculadora Básica</h3>
+    
+    <div class="calculadora-grilla">
+
+        <div class="output">
+            <input class="display" type="text" id="result" placeholder="0">
+        </div>
+
+        <button name="data-number">1</button>
+        <button name="data-number">2</button>
+        <button name="data-number">3</button>
+        <button name="data-opera">+</button>
+        <button name="data-number">4</button>
+        <button name="data-number">5</button>
+        <button name="data-number">6</button>
+        <button name="data-opera">-</button>
+        <button name="data-number">7</button>
+        <button name="data-number">8</button>
+        <button name="data-number">9</button>
+        <button name="data-opera">/</button>
+        <button name="data-delete">C</button>
+        <button name="data-number">0</button>
+        <button name="data-igual">=</button>
+        <button name="data-opera">x</button>
+
+    </div>
+    
+    
+    
+
+</body>
+</html>
